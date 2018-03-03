07ecc206d1b487fee477be528748d43d7236d755
sun-strider <27220927+sun-strider@users.noreply.github.com>
2018-03-03 15:28:58 +0100

    Populated the override methods in MovieAdapter

--- app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.popularmovies;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -11,28 +12,60 @@ import android.widget.TextView;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
 
+    private String[] mMovieData;
+
     @Override
     public MovieAdapter.MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        // get the layout for the movie list item
+        int layoutIdForListItem = R.layout.movie_list_item;
+        // get the layout inflater
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        // bool that decides attachment behaviour
+        boolean attachToParentNow = false;
+
+        // inflate the view with the above info
+        View view = inflater.inflate(layoutIdForListItem, parent, attachToParentNow);
+
+        // return new view holder and pass in the inflated view
+        return new MovieViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MovieAdapter.MovieViewHolder holder, int position) {
 
+        // get the current movie information
+        String currentMovie = mMovieData[position];
+
+        // get the movie item text view from the holder and set the information of the current movie
+        holder.movieListItemTextView.setText(currentMovie);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        // check if movie data exists. if not return 0 as item count, else return the length of the array
+        if (mMovieData == null) {
+            return 0;
+        }
+        return mMovieData.length;
+    }
+
+    // a method that sets the movie data to mMovieData
+    void setMovieData(String movieData[]) {
+        mMovieData = movieData;
+        notifyDataSetChanged();
     }
 
     class MovieViewHolder extends RecyclerView.ViewHolder {
 
-        final TextView mMovieTextView;
+        // create a TextView variable in which the movie data will be stored
+        final TextView movieListItemTextView;
 
         public MovieViewHolder(View itemView) {
             super(itemView);
-            mMovieTextView = itemView.findViewById(R.id.tv_movie_data);
+
+            // in the constructor of the view holder, the movie item view is set
+            movieListItemTextView = itemView.findViewById(R.id.tv_movie_data);
         }
     }
 }
